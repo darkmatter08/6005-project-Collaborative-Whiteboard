df34b4c91f18f8e98c094b059aafeaed69b4bf39
Blake Elias <blakeelias@gmail.com>
2013-11-25 16:05:58 -0500

    Add getter and creator methods for Canvases in Server

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,7 +1,36 @@
 package server;
 
-public class Server {
+import java.util.*;
+import canvas.*;
 
+public class Server {
+    private ArrayList<Canvas> whiteboards;
+    
+    public Server() {
+        whiteboards = new ArrayList<Canvas>();
+    }
+    
+    /**
+     * Retrieve a canvas with a particular ID number
+     * @param id The index of the desired canvas in the server's list
+     * @return A Canvas object
+     */
+    public Canvas getCanvasByID(int id) {
+        return whiteboards.get(id);
+    }
+    
+    /**
+     * Create a new canvas and return its ID number.
+     * @param width The width of the canvas to create, in pixels
+     * @param height The height of the canvas to create, in pixels
+     * @return The ID number of the newly created canvas
+     */
+    public synchronized int newCanvasID(int width, int height) {
+        Canvas c = new Canvas(width, height);
+        whiteboards.add(c);
+        return whiteboards.size() - 1;
+    }
+    
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
