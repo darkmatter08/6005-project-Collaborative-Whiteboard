ce3d650996affaad8925f4312f8b43c6d27fe9ee
Blake Elias <blakeelias@gmail.com>
2013-11-25 16:42:49 -0500

    Make Server implement WhiteBoardServer interface

--- src/server/Server.java
+++ src/server/Server.java
@@ -3,11 +3,19 @@ package server;
 import java.util.*;
 import canvas.*;
 
-public class Server {
+public class Server implements canvas.WhiteBoardServer {
     private ArrayList<Canvas> whiteboards;
     
     public Server() {
         whiteboards = new ArrayList<Canvas>();
+    }
+    
+    public List<Integer> getWhiteBoardIds() {
+        ArrayList<Integer> ids = new ArrayList<Integer>();
+        for (int i = 0; i < whiteboards.size(); ++i) {
+            ids.add(i);
+        }
+        return ids;
     }
     
     /**
@@ -25,10 +33,20 @@ public class Server {
      * @param height The height of the canvas to create, in pixels
      * @return The ID number of the newly created canvas
      */
-    public synchronized int newCanvasID(int width, int height) {
+    public synchronized int createNewWhiteBoard(int width, int height) {
         Canvas c = new Canvas(width, height);
         whiteboards.add(c);
         return whiteboards.size() - 1;
+    }
+    
+    /**
+     * Create a new canvas with dimensions 800 (width) by 600 (height) and return its ID number.
+     * @param width The width of the canvas to create, in pixels
+     * @param height The height of the canvas to create, in pixels
+     * @return The ID number of the newly created canvas
+     */
+    public synchronized int createNewWhiteBoard() {
+        return createNewWhiteBoard(800, 600);
     }
     
     public static void main(String[] args) {
