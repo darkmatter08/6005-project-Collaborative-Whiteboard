8cd0e518b4538469e397962c69e1d75bdeba0ace
Blake Elias <blakeelias@gmail.com>
2013-12-02 18:44:37 -0500

    Create WhiteboardAction and Whiteboard.applyAction()

--- src/canvas/Whiteboard.java
+++ src/canvas/Whiteboard.java
@@ -1,7 +1,10 @@
 package canvas;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Stroke;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
 
@@ -10,5 +13,16 @@ public class Whiteboard {
     
     public Whiteboard(Image drawingBuffer) {
         this.drawingBuffer = drawingBuffer;
+    }
+    
+    /*
+     * Draw a line between two points (x1, y1) and (x2, y2), specified in
+     * pixels relative to the upper-left corner of the drawing buffer.
+     */
+    public void applyAction(WhiteboardAction action) {
+        Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
+        g.setColor(action.color);
+        g.setStroke(action.stroke);
+        g.drawLine(action.x1, action.y1, action.x2, action.y2);
     }
 }
---
+++ src/canvas/WhiteboardAction.java
@@ -0,0 +1,13 @@
+package canvas;
+
+import java.awt.Color;
+import java.awt.Stroke;
+
+public class WhiteboardAction {
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    Color color;
+    Stroke stroke;
+}
