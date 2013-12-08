009a52d11f42d4f667bf1eafcc3f9d10cc574b9b
Shawn Jain <shawnjain.08@gmail.com>
2013-12-08 17:44:24 -0500

    updated masterwhiteboard

--- src/server/MasterWhiteboard.java
+++ src/server/MasterWhiteboard.java
@@ -5,11 +5,31 @@ import shared.*;
 import java.util.concurrent.BlockingQueue;
 
 /**
- * The entire history of a whiteboard object, stored internally
- * as a queue of WhiteboardActions.
- * @author eliasb
+ * Represents the state of a whiteboard with a unique id, as a complete 
+ * history of actions applied to the whiteboard, as a queue of WhiteboardActions.
+ * @author eliasb jains
  *
  */
 public class MasterWhiteboard {
-    BlockingQueue<WhiteboardAction> history;
+    private BlockingQueue<WhiteboardAction> history;
+    final private int id;
+    
+    /**
+     * Initializes new blank whiteboard
+     */
+    public MasterWhiteboard(int id) {
+        this.id = id;
+    }
+    
+    public synchronized void applyAction(WhiteboardAction action) {
+        history.add(action);
+    }
+    
+    public BlockingQueue<WhiteboardAction> getHistory() {
+        return history;
+    }
+    
+    public int getId() {
+        return id;
+    }
 }
