50d4efbd98e7b895ebc114d418b7e2a6d6c90699
Blake Elias <blakeelias@gmail.com>
2013-12-08 19:20:32 -0500

    Two threads in SlaveServer for sending and receiving actions

--- src/server/SlaveServer.java
+++ src/server/SlaveServer.java
@@ -23,6 +23,7 @@ public class SlaveServer implements Runnable {
     // IO
     private ObjectOutputStream objOut = null;
     private ObjectInputStream objIn = null;
+    private int lastHistorySize;
     
     /**
      * Constructor for a new ConnectionHandler. 
@@ -32,42 +33,84 @@ public class SlaveServer implements Runnable {
     public SlaveServer(MasterWhiteboard whiteboard, Socket socket) {
         this.whiteboard = whiteboard;
         this.socket = socket;
+        lastHistorySize = 0;
     }
     
     public void run() {
         // handle the client
-        try {
-            handleConnection();
-        } catch (IOException e) {
-            e.printStackTrace(); // but don't terminate serve()
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+        
+        // receive client draw actions
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    receiveClientActions();
+                } catch (IOException e) {
+                    e.printStackTrace(); // but don't terminate serve()
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
             }
-        }
+        }.start();
+        
+        // send client draw actions
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sendActions();
+                } catch (IOException e) {
+                    e.printStackTrace(); // but don't terminate serve()
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
 
-    private void handleConnection() throws IOException {
-        this.objOut = new ObjectOutputStream(socket.getOutputStream());
+    private void receiveClientActions() throws IOException {
         this.objIn = new ObjectInputStream(socket.getInputStream());
         
         try {
             for (List<WhiteboardAction> actions = (List<WhiteboardAction>)objIn.readObject(); actions != null;
                     actions = (List<WhiteboardAction>)objIn.readObject()) {
-                handleRequest(actions);
+                whiteboard.applyActions(actions);
             }
         } catch (Exception e){
             e.printStackTrace();
         } finally {
-            objOut.close();
             socket.close();
         }
     }
     
-    private void handleRequest(List<WhiteboardAction> actions) {
-        whiteboard.applyActions(actions);
+    private void sendActions() throws IOException {
+        this.objOut = new ObjectOutputStream(socket.getOutputStream());
+        
+        try {
+            for (List<WhiteboardAction> actions = (List<WhiteboardAction>)objIn.readObject(); actions != null;
+                    actions = (List<WhiteboardAction>)objIn.readObject()) {
+                int newHistorySize;
+                synchronized (whiteboard) {
+                    newHistorySize = whiteboard.getHistory().size();
+                }
+                List<WhiteboardAction> newActions = ((List<WhiteboardAction>)whiteboard.getHistory()).subList(lastHistorySize, newHistorySize - 1);
+                objOut.writeObject(newActions);
+                this.lastHistorySize = newHistorySize;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            socket.close();
+        }
     }
 }
