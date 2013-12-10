26d53fe45fff4cb8ceaa172c34ddea67bf5306b2
Shawn Jain <shawnjain.08@gmail.com>
2013-12-10 15:40:26 -0500

    fixed tests for will's server, updated TestUtility

--- src/test/MasterServerStarterTest.java
+++ src/test/MasterServerStarterTest.java
@@ -1,58 +1,82 @@
 package test;
 
 import static org.junit.Assert.*;
-
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
+import shared.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * Tests both the MasterServerStarter (to establish the connection)
+ *  and the MasterServer to send and receive messages. 
+ * Sets up one continuous server instance and then closes the sockets 
+ *  after the jUnit is completed. 
+ * @author jains
+ *
+ */
 public class MasterServerStarterTest {
     
-    private Socket socket = null;
-    private ObjectOutputStream objOut;
-    private ObjectInputStream objIn;
-    private BufferedReader in; 
-    private PrintWriter out; 
+    private static Socket socket;
+    private static BufferedReader in; 
+    private static PrintWriter out; 
     
-    @Before
-    public void setup() throws IOException, InterruptedException {
-        TestUtility.startServer(); // starts MasterServerStarter
+    @BeforeClass
+    public static void setup() throws IOException {
+        TestUtility.startServer();  
         socket = TestUtility.connect();
-        Thread.sleep(300);
         
-        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        this.objIn = new ObjectInputStream(socket.getInputStream());
-        this.objOut = new ObjectOutputStream(socket.getOutputStream());
-        this.out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+        System.out.println("done setup");
+    }
+    
+    @AfterClass
+    public static void teardown() throws IOException, InterruptedException {
+        in.close();
+        out.close();
+        socket.close();
+        System.out.println("done teardown");
+        Thread.sleep(500);
     }
     
     @Test
-    public void testServerStart() throws IOException, InterruptedException {
-        Thread.sleep(300);
-        System.out.println(this.out);
-        out.write("createNewWhiteboard\n");
-        System.out.println("sent createNewWhiteboard");
-        try {
-            while(objIn.available() == 0){
-            }
-            List<Integer> allWhiteboards = (List<Integer>) objIn.readObject();
-            List<Integer> expected = new ArrayList<Integer>();
-            expected.add(1);
-            assert(allWhiteboards.equals(expected));
-        } catch (ClassNotFoundException e) {
-            fail("unexpected object returned");
-        }
+    public void fullTest() throws IOException {
+        testCreateNewWhiteboard();
+        testGetWhiteboardIds();
+        testManyNewWhiteboards();
     }
     
+    public void testCreateNewWhiteboard() throws IOException {
+        out.println(Messages.CREATE_NEW_WHITEBOARD);
+
+        int allWhiteboards = Integer.parseInt(in.readLine());
+        assertEquals(allWhiteboards, 1);
+    }
+    
+    public void testGetWhiteboardIds() throws IOException {
+        out.println(Messages.ASK_FOR_WHITEBOARDS);
+        
+        int allWhiteboards = Integer.parseInt(in.readLine());
+        assertEquals(allWhiteboards, 1);
+    }
+     
+    public void testManyNewWhiteboards() throws IOException {        
+        //Board 2
+        out.println(Messages.CREATE_NEW_WHITEBOARD);
+
+        int allWhiteboards = Integer.parseInt(in.readLine());
+        assertEquals(allWhiteboards, 2);
+        
+        out.println(Messages.CREATE_NEW_WHITEBOARD);
+        
+        allWhiteboards = Integer.parseInt(in.readLine());
+        assertEquals(allWhiteboards, 3);
+    }
 
     
 }
