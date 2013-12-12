993e2a96f8f7cfad276e0e9c0de3427a77450d1a
Blake Elias <blakeelias@gmail.com>
2013-12-11 23:46:15 -0500

    Add comments to PickerClientConnectionHandler.java

--- src/canvas/PickerClientConnectionHandler.java
+++ src/canvas/PickerClientConnectionHandler.java
@@ -13,6 +13,10 @@ import javax.swing.SwingUtilities;
 
 import shared.ConnectionDetails;
 
+/**
+ * Represents the connection of the picker client (program to set username and choose
+ * whiteboard) and the server.
+ */
 public class PickerClientConnectionHandler {
 	private WhiteBoardTableModel tableModel;
 	private WhiteboardPickerClient parentFrame;
@@ -25,6 +29,9 @@ public class PickerClientConnectionHandler {
 		this.tableModel = tableModel;
 	}
 
+	/*
+	 * Initialize connection between client and server.
+	 */
 	public synchronized void init() {
 		try {
 			mySocket = new Socket(ConnectionDetails.SERVER_ADDRESS, ConnectionDetails.CLIENT_PICKER_GUI_PORT);
@@ -37,15 +44,26 @@ public class PickerClientConnectionHandler {
 		}
 	}
 
+	/**
+	 * Ask for the latest list of whiteboard IDs.
+	 * 
+	 * Called in order to send a message to ask for list of whiteboard IDs.
+	 */
 	@SuppressWarnings("unchecked")
 	public void askForWhiteboardIds() {
 		out.println(shared.Messages.ASK_FOR_WHITEBOARDS);
 	}
 
+	/**
+	 * Ask to create a new whiteboard on the server
+	 */
 	public synchronized void createNewWhiteBoard() {
 		out.println(shared.Messages.CREATE_NEW_WHITEBOARD);
 	}
 
+	/**
+	 * Display latest list of whiteboards from server in selection list.
+	 */
 	public void watchForNewWhiteboards() {
 		new Thread() {
 			@SuppressWarnings("unchecked")
@@ -66,6 +84,11 @@ public class PickerClientConnectionHandler {
 		}.start();
 	}
 
+	/**
+	 * Open a whiteboard in a new window.
+	 * @param boardId The ID of the board to open.
+	 * @param username The username with which to connect to this whiteboard.
+	 */
 	public void openWhiteboard(final int boardId, final String username) {
 		new Thread() {
 			public void run() {
